a4118ac04c558d30f7e65ef2d3dc68dcb4371643
Aman Ketebo <amanketebo@yahoo.com>
2020-12-19 01:08:48 -0500

    Small clean up

--- Sources/postgreshelper/main.swift
+++ Sources/postgreshelper/main.swift
@@ -1,24 +1,16 @@
 import Foundation
-import ArgumentParser
 
-struct PostgresSetUp: ParsableCommand {
-    @Argument(help: "The config's file path")
-    var configAbsolutePath: String
+let sourceRootAbsolutePath = ProcessInfo.processInfo.environment["SRCROOT"] ?? ""
+let config = ConfigParser.parse(from: sourceRootAbsolutePath + "/postgres-local-database-config.json")
+let postgresDatabase = PostgresDatabase(config: config)
+let buildScriptChecker = BuildScriptChecker(config: config,
+                                            sourceRootAbsolutePath: sourceRootAbsolutePath)
 
-    mutating func run() throws {
-        let config = ConfigParser.parse(from: configAbsolutePath)
-        let postgresDatabase = PostgresDatabase(config: config)
-        let buildScriptChecker = BuildScriptChecker(config: config)
-
-        if buildScriptChecker.shouldUpdateTables() {
-            if buildScriptChecker.savedBuildScriptString().isEmpty {
-                postgresDatabase.dropAllTables()
-            }
-
-            postgresDatabase.addTables()
-            buildScriptChecker.saveCurrentBuildScriptString()
-        }
+if buildScriptChecker.shouldUpdateTables() {
+    if !buildScriptChecker.savedBuildScriptString().isEmpty {
+        postgresDatabase.dropAllTables()
     }
-}
 
-PostgresSetUp.main()
+    postgresDatabase.addTables()
+    buildScriptChecker.saveCurrentBuildScriptString()
+}
