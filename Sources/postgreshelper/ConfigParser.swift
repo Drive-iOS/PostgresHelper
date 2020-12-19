b5331df6635200c3d02e512aac2435e2e880f1c8
Aman Ketebo <amanketebo@yahoo.com>
2020-12-19 03:09:28 -0500

    Print out error when parsing config fails

--- Sources/postgreshelper/ConfigParser.swift
+++ Sources/postgreshelper/ConfigParser.swift
@@ -10,10 +10,13 @@ import Foundation
 enum ConfigParser {
     static func parse(from absolutePath: String) -> Config {
         let configURL = URL(fileURLWithPath: absolutePath)
-
-        guard let configData = try? Data(contentsOf: configURL),
-              let config = try? JSONDecoder().decode(Config.self, from: configData) else {
+        let config: Config
+        do {
+            let configData = try Data(contentsOf: configURL)
+            config = try JSONDecoder().decode(Config.self, from: configData)
+        } catch {
             print("❌ Failed to parse config")
+            print("- \(error.localizedDescription)")
             exit(1)
         }
 
